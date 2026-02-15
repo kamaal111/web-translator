2fcf035ccecbe27bb58c3dcc107b8b1ddad04151
Kamaal Farah <kamaal.f1@gmail.com>
2026-02-15 15:21:45 +0100

    Setting up bulk editor page

--- web/src/routing/router.tsx
+++ web/src/routing/router.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router';
 import LazyHome from '@/pages/home/lazy-home';
 import LazyLogin from '@/pages/login/lazy-login';
 import LazyProject from '@/pages/project/lazy-project';
+import LazyBulkEditor from '@/pages/bulk-editor/lazy-bulk-editor';
 import PageLayout from '@/layouts/page-layout/page-layout';
 import LoginRequiredLayout from '@/layouts/login-required-layout/login-required-layout';
 
@@ -13,6 +14,7 @@ function Router() {
         <Route element={<LoginRequiredLayout />}>
           <Route index element={<LazyHome />} />
           <Route path="/projects/:id" element={<LazyProject />} />
+          <Route path="/projects/:id/bulk-editor" element={<LazyBulkEditor />} />
         </Route>
         <Route path="/login" element={<LazyLogin />} />
       </Route>
